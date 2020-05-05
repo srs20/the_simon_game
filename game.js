3b16e1887e7c9e763bd550678ab2300f8d97c945
Smruti Ranjan Swain <56722613+srs20@users.noreply.github.com>
2020-05-05 22:27:24 +0530

    initial

---
+++ game.js
@@ -0,0 +1,76 @@
+var gamePattern=[];
+var userClickedPattern=[];
+var buttonColours=["red", "blue", "green", "yellow"];
+var level=0;
+var started=false;
+
+$(".btn").click(function(){
+    var userChosenColour=$(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    checkAnswer((userClickedPattern.length)-1);
+});
+
+$(document).keypress(function(){
+    if(!started){
+        $("#level-title").html("Level "+level);
+        nextSequence();
+        started=true;
+    }
+    
+});
+
+function nextSequence(){
+    userClickedPattern.length=0;
+    var randomNumber=Math.floor(Math.random()*4);
+    var randomChosenColour=buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    $("#"+randomChosenColour).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+    level++;
+    $("#level-title").html("Level "+level);    
+}
+
+function playSound(name){
+    var audio=new Audio("sounds/"+name+".mp3");
+    audio.play();
+}
+
+function animatePress(currentColor){
+    $("#"+currentColor).addClass("pressed");
+    setTimeout(function(){
+        $("#"+currentColor).removeClass("pressed");
+    },100);
+}
+
+function checkAnswer(currentLevel){
+    if(gamePattern[currentLevel]===userClickedPattern[currentLevel]){
+        console.log("Success");
+        if(userClickedPattern.length===gamePattern.length){
+            setTimeout(function(){
+                nextSequence();
+            },1000);
+        }
+    }
+    else{
+        console.log("failed");
+        $("#level-title").html("Game Over! Press any key to Restart");
+        playSound("wrong");
+        $("body").addClass("game-over");
+        setTimeout(function(){
+            $("body").removeClass("game-over");
+        },200);
+        startOver();
+    }
+
+    function startOver(){
+        level=0;
+        gamePattern.length=0;
+        started=false;
+    }
+}
+
+
+
+
